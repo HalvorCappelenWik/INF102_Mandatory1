3e823ca33d9793dd480732884450477de767422e
Halvor Cappelen Wik <halvorcappelenwik@Halvors-MacBook-Pro.local>
2022-09-18 17:48:35 +0200

    Finished with remove random

--- src/main/java/INF102/Mandatory1/util/MyRandomSelector.java
+++ src/main/java/INF102/Mandatory1/util/MyRandomSelector.java
@@ -1,5 +1,6 @@
 package INF102.Mandatory1.util;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
@@ -9,12 +10,22 @@ public class MyRandomSelector implements IRandomSelector {
 
 	@Override
 	public <T> T removeRandom(List<T> list) {
-		throw new UnsupportedOperationException();
+		if (list.size() == 0) throw new IllegalArgumentException("List is empty");
+		return list.remove(random.nextInt(list.size()));
 	}
 
 	@Override
 	public <T> List<T> removeRandom(List<T> list, int k) {
-		throw new UnsupportedOperationException();
+		if (list.size() == 0) throw new IllegalArgumentException("List is empty");
+		List<T> removedItems = new LinkedList<T>();
+
+		while(k > 0) {
+			if (list.size() < k) throw new IllegalArgumentException("Not enough items in list");
+			removedItems.add(removeRandom(list));
+			k --;
+		}
+		list.removeAll(removedItems);
+		return removedItems;
 
 	}
 	
