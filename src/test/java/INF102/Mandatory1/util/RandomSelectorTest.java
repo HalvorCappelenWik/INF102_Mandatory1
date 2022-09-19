4225dbf456777919de932368468df67d874ff7ef
Halvor Cappelen Wik <halvorcappelenwik@Halvors-MacBook-Pro.local>
2022-09-19 15:37:46 +0200

    Added new random test

--- src/test/java/INF102/Mandatory1/util/RandomSelectorTest.java
+++ src/test/java/INF102/Mandatory1/util/RandomSelectorTest.java
@@ -50,7 +50,7 @@ public class RandomSelectorTest {
 
 	@Test
 	public void testKElementsremovedFromList() {
-		for(int k=1; k<list.size(); k++) {
+		for (int k = 1; k < list.size(); k++) {
 			testKElementsremovedFromList(k);
 		}
 	}
@@ -58,59 +58,65 @@ public class RandomSelectorTest {
 	public void testKElementsremovedFromList(int k) {
 		int size = list.size();
 		List<String> copyList = (List<String>) list.clone();
-		List<String> elem = selector.removeRandom(list,k);
+		List<String> elem = selector.removeRandom(list, k);
 		assertEquals(size - k, list.size(),
-				"when removeing "+k+" elements, list size should decrease by "+k);
-		for(String s : elem) {
+				"when removeing " + k + " elements, list size should decrease by " + k);
+		for (String s : elem) {
 			int countBefore = Collections.frequency(copyList, s);
 			int countAfter = Collections.frequency(list, s);
 			int countRemoved = Collections.frequency(elem, s);
 			assertEquals(countBefore - countRemoved, countAfter,
-				"The elements returned was not the same as the elements removed");
+					"The elements returned was not the same as the elements removed");
 		}
 		for (String str : list) {
 			if (elem.contains(str))
 				continue;
 			assertEquals(Collections.frequency(copyList, str), Collections.frequency(list, str),
 					"Removed an element from list that was not returned");
-		}		
+		}
 	}
-	
+
 	@Test
 	void testDistributionIsRandom() {
-		//1000 repeats with probability 1/10 gives expected count of 100
-		//Having an expected count of 65 or less happens once in 20000 times
+		// 1000 repeats with probability 1/10 gives expected count of 100
+		// Having an expected count of 65 or less happens once in 20000 times
 		List<String> list = StringListGenerator.generateStringList(10);
 		HashMap<String, Integer> count = new HashMap<>();
+		for (String s : list)
+			count.put(s, 0);
+
 		IRandomSelector selector = new MyRandomSelector();
-		for(int i=0; i<1000; i++) {
+		for (int i = 0; i < 1000; i++) {
 			String rand = selector.removeRandom(new ArrayList<>(list));
-			count.put(rand, count.getOrDefault(rand, 0)+1);
+			count.put(rand, count.get(rand) + 1);
 		}
-		
-		for(String s : count.keySet()) {
-			assertTrue(count.get(s)>65,"index "+list.indexOf(s)+" was selected too few times");
+
+		for (String s : count.keySet()) {
+			assertTrue(count.get(s) > 65, "index " + list.indexOf(s) + " was selected too few times");
 		}
 	}
-	
+
 	@Test
 	void testDistributionIsRandomK() {
-		//1000 repeats with probability 1/10 gives expected count of 100
-		//Having an expected count of 65 or less happens once in 20000 times
+		// 1000 repeats with probability 1/10 gives expected count of 100
+		// Having an expected count of 65 or less happens once in 20000 times
 		List<String> list = StringListGenerator.generateStringList(10);
 		HashMap<String, Integer> count = new HashMap<>();
+		for (String s : list)
+			count.put(s, 0);
+
 		IRandomSelector selector = new MyRandomSelector();
-		for(int i=0; i<250; i++) {
-			List<String> rand = selector.removeRandom(new ArrayList<>(list),4);
-			for(String s : rand)
-				count.put(s, count.getOrDefault(s, 0)+1);
+		for (int i = 0; i < 250; i++) {
+			List<String> rand = selector.removeRandom(new ArrayList<>(list), 4);
+			for (String s : rand)
+				count.put(s, count.get(s) + 1);
 		}
-		
-		for(String s : count.keySet()) {
-			assertTrue(count.get(s)>65,"index "+list.indexOf(s)+" was selected too few times");
+
+		for (String s : count.keySet()) {
+			assertTrue(count.get(s) > 65, "index " + list.indexOf(s) + " was selected too few times");
 		}
 	}
-	
+
 	@Test
 	void testNotEnoughElements() {
 		assertThrows(IllegalArgumentException.class, () -> selector.removeRandom(new ArrayList<String>()));
@@ -118,7 +124,5 @@ public class RandomSelectorTest {
 		List<String> tenStrings = StringListGenerator.generateStringList(10);
 		assertThrows(IllegalArgumentException.class, () -> selector.removeRandom(tenStrings, 11));
 	}
-	
-	
 
 }
