0ad3d2d3d8051d0ffd42928fbf4dbda9b56122dd
Halvor Cappelen Wik <halvorcappelenwik@pc139-196.eduroam.uib.no>
2022-09-21 10:31:59 +0200

    Startet med å lage closestcomparator

---
+++ src/main/java/INF102/Mandatory1/management/strategies/ClosestComparator.java
@@ -0,0 +1,13 @@
+package INF102.Mandatory1.management.strategies;
+
+import INF102.Mandatory1.management.Robot;
+
+import java.util.Comparator;
+
+public class ClosestComparator implements Comparator<Robot> {
+    
+    @Override
+    public int compare(Robot o1, Robot o2) {
+        return 0;
+    }
+}
